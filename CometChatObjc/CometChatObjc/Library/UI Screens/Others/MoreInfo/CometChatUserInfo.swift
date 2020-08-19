a52432c907002547059d0b2630463055e022f81d
pushpsenairekar2911 <pushpsenairekar@Pushpsens-Mac-mini.local>
2020-08-19 17:31:39 +0530

    v2.1.0

--- CometChatObjc/CometChatObjc/Library/UI Screens/Others/MoreInfo/CometChatUserInfo.swift
+++ CometChatObjc/CometChatObjc/Library/UI Screens/Others/MoreInfo/CometChatUserInfo.swift
@@ -90,7 +90,7 @@ public class CometChatUserInfo: UIViewController {
               self.present(alert, animated: true, completion: nil)
           }
           let user = User(uid: uid, name: name)
-          CometChat.updateUser(user: user, apiKey: "Enter Auth Key", onSuccess: { (user) in
+          CometChat.updateUser(user: user, apiKey: "Enter API KEY", onSuccess: { (user) in
               DispatchQueue.main.async {
                   self.dismiss(animated: true, completion: nil)
                   self.tableView.reloadData()
